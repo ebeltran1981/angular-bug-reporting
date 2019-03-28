d3473189ab78dc4ad2006d0fd177a9e407bb9048
Edwin Beltran <edwin.beltran@me.com>
2019-03-28 12:47:38 -0400

    demo of multiselect label problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testApp';
+  title = 'Angular Sample for Bug Reporting';
+
+  formGroup = new FormGroup({
+    multiselectId: new FormControl('', Validators.required),
+    dropdownId: new FormControl('', Validators.required),
+  });
+
+  dataSource = [
+    {
+      id: 1,
+      text: 'Option 1'
+    },
+    {
+      id: 2,
+      text: 'Option 2'
+    }
+  ];
+
+  fieldMapping = {
+    value: 'id',
+    text: 'text'
+  };
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      alert('form submitted');
+    } else {
+      alert('form is invalid');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultiSelectModule, DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MultiSelectModule,
+    DropDownListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
